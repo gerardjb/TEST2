b8b500265464fa3853efea489d784085c69e5b28
David Turner <dmturner@princeton.edu>
2024-06-03 15:25:44 -0400

    Refactor

---
+++ src/spike_find/pgas/include/Analyzer.h
@@ -0,0 +1,36 @@
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+#include <string>
+
+class Analyzer {
+public:
+    Analyzer(const std::string& data_file, const std::string& constants_file, const std::string& output_folder,
+                 unsigned int column, const std::string& tag, unsigned int niter = 0, const std::string& trainedPriorFile = "",
+                 bool append = false, unsigned int trim = 1, bool verbose = true, const std::string& gtSpike_file = "",
+                 bool has_trained_priors = false, bool has_gtspikes = false, unsigned int maxlen = 0, const std::string& Gparam_file = "");
+
+    void run();
+		std::vector<double> final_params;
+
+private:
+    std::string data_file;
+    std::string constants_file;
+    std::string output_folder;
+    unsigned int column;
+    std::string gtSpike_file;
+    std::string tag;
+
+    std::string trainedPriorFile;
+    bool has_trained_priors;
+    bool has_gtspikes;
+    std::string Gparam_file;
+    bool append;
+    unsigned int niter;
+    unsigned int trim;
+    bool verbose;
+    unsigned int maxlen;
+		
+};
+
+#endif // ANALYZER_H
---
+++ src/spike_find/pgas/include/param.h
@@ -0,0 +1,29 @@
+#ifndef PARAM_H
+#define PARAM_H
+
+#include<fstream>
+#include "constants.h"
+#include "../include/reparam.h"
+
+class param {
+public:
+    double G_tot;
+    double gamma;
+    double DCaT;
+    double Rf;
+    double gam_in;
+    double gam_out;
+    double r0, r1;
+    double* wbb;
+    double sigma2;
+    void print();
+    void write(ofstream&,double);
+    param& operator=(const param&);
+    string filename;
+    param();
+    param(string);
+    double logPrior(const constpar&);
+
+};
+
+#endif
